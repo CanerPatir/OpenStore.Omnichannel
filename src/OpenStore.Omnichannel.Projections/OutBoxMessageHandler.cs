78d79dcbff15ca32ccd30caba383ef9dda15bc40
Caner Patır <caner.patir@trendyol.com>
2021-09-30 01:15:44 +0300

    .

--- src/OpenStore.Omnichannel.Projections/OutBoxMessageHandler.cs
+++ src/OpenStore.Omnichannel.Projections/OutBoxMessageHandler.cs
@@ -1,19 +1,23 @@
 using System.Linq;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using OpenStore.Data.OutBox;
-using OpenStore.Infrastructure.Messaging.Kafka;
 
 namespace OpenStore.Omnichannel.Projections
 {
     public class OutBoxMessageHandler : BaseOutBoxMessageHandler
     {
-        private readonly KafkaProducer _producer;
+        // private readonly KafkaProducer _producer;
+        private readonly ILogger<OutBoxMessageHandler> _logger;
 
-        public OutBoxMessageHandler(KafkaProducer producer)
+        public OutBoxMessageHandler(
+            // KafkaProducer producer,
+            ILogger<OutBoxMessageHandler> logger
+        )
         {
-            _producer = producer;
+            // _producer = producer;
+            _logger = logger;
         }
 
         // todo: monitor performance
@@ -21,10 +25,11 @@ namespace OpenStore.Omnichannel.Projections
         {
             foreach (var outBoxMessagesByAggregateId in outBoxMessageBatch.Messages.GroupBy(x => x.AggregateId))
             {
-                await _producer.ProduceMany(ServiceCollectionExtensions.OpenStoreOutboxTopic,
-                    outBoxMessagesByAggregateId.Key, 
-                    outBoxMessagesByAggregateId.ToList(),
-                    cancellationToken);
+                _logger.LogInformation($"{outBoxMessageBatch.Messages.Count} retrieved");
+                // await _producer.ProduceMany(ServiceCollectionExtensions.OpenStoreOutboxTopic,
+                //     outBoxMessagesByAggregateId.Key, 
+                //     outBoxMessagesByAggregateId.ToList(),
+                //     cancellationToken);
             }
         }
     }
--- src/OpenStore.Omnichannel.Projections/ServiceCollectionExtensions.cs
+++ src/OpenStore.Omnichannel.Projections/ServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OpenStore.Data.OutBox;
@@ -15,11 +18,26 @@ namespace OpenStore.Omnichannel.Projections
             , IConfigurationSection kafkaConfigSection
             , IConfigurationSection elasticSearchConfigSection)
         {
-            services.AddKafkaProducer(kafkaConfigSection);
-            services.AddKafkaConsumer<OutBoxMessageConsumer, OutBoxMessage>(OpenStoreOutboxTopic, kafkaConfigSection);
-            services.AddElasticSearch(elasticSearchConfigSection);
-            // services.AddHostedService<TestWorker>();
+            // services.AddKafkaProducer(kafkaConfigSection);
+            // services.AddKafkaConsumer<OutBoxMessageConsumer, OutBoxMessage>(OpenStoreOutboxTopic, kafkaConfigSection);
+            // services.AddElasticSearch(elasticSearchConfigSection);
+            //// services.AddHostedService<TestWorker>();
 
+            // Todo: run on app init
+            // using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            // {
+            //     try
+            //     {
+            //          adminClient.CreateTopicsAsync(new[] { 
+            //             new TopicSpecification { Name = OpenStoreOutboxTopic, ReplicationFactor = 2, NumPartitions = 20 } })
+            //              .ConfigureAwait(false).GetAwaiter().GetResult();
+            //     }
+            //     catch (CreateTopicsException e)
+            //     {
+            //         Console.WriteLine($"An error occured creating topic {e.Results[0].Topic}: {e.Results[0].Error.Reason}");
+            //     }
+            // }
+            
             return services;
         }
     }
